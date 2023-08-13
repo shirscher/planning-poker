48f92146e16c6cc2ee330243a409f73aca4369b8
Steve Hirscher <shirscher@gmail.com>
2023-08-12 22:06:44 -0500

    Add more animations and interactions

--- frontend/js/game.js
+++ frontend/js/game.js
@@ -12,24 +12,36 @@ const gameSettings = {
             "name": "Fibonacci",
             "values": [
                 {
-                    "value": 1,
-                    "text": "1"
+                    "value": "1",
                 },
                 {
-                    "value": 2,
-                    "text": "2"
+                    "value": "2",
                 },
                 {
-                    "value": 3,
-                    "text": "3"
+                    "value": "3",
                 },
                 {
-                    "value": 5,
-                    "text": "5"
+                    "value": "5",
                 },
                 {
-                    "value": 8,
-                    "text": "8"
+                    "value": "8",
+                },
+                {
+                    "value": "13",
+                },
+                {
+                    "value": "21",
+                },
+                {
+                    "value": "100",
+                },
+                {
+                    "value": "infinity",
+                    "html": "∞"
+                },
+                {
+                    "value": "coffee",
+                    "html": "☕"
                 }
             ]
         }
